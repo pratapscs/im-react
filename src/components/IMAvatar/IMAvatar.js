ca30f7cabf8d8772625a595a9b153a5b1915c410
pratap <pratap@zkteco.in>
2021-01-22 11:50:44 +0530

    adding avatar component

---
+++ src/components/IMAvatar/IMAvatar.js
@@ -0,0 +1,13 @@
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+
+
+/** SVG Eye Icon */
+function IMAvatar() {
+  // Attribution: Fabián Alexis at https://commons.wikimedia.org/wiki/File:Antu_view-preview.svg
+  return (
+    <Avatar>V</Avatar>
+  )
+}
+
+export default IMAvatar;
